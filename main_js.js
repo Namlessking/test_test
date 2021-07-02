4563063663b77c633a07c83882eaec100430d226
taoufiq <protips234@gmail.com>
2021-07-02 13:55:11 -0800

    unpdate

--- main_js.js
+++ main_js.js
@@ -7,32 +7,40 @@ input_container.addEventListener('click', delete_task);
 
 function new_task(event) {
     event.preventDefault();
-    const newdiv = document.createElement('div')
+    const newdiv = document.createElement('div');
     newdiv.classList = "newdiv";
-    const newli = document.createElement('li')
+    const newli = document.createElement('input');
     newli.classList = "newlit";
-    newli.innerText = input_zone.value;
+    newli.value = input_zone.value;
+    localStorage.setItem('Task', input_zone.value);
+    newli.disabled = true;
     newdiv.appendChild(newli);
     const addbtn = document.createElement('button')
     addbtn.classList = "newadd";
-    addbtn.innerHTML = '<i class="fas fa-plus"></i>'
+    addbtn.innerHTML = '<i class="fas fa-plus"></i>';
     newdiv.appendChild(addbtn);
-    const deletbtn = document.createElement('button')
+    const changebtn = document.createElement('button');
+    changebtn.classList = "changebtn";
+    changebtn.innerHTML = '<i class="fas fa-carrot"></i>';
+    newdiv.appendChild(changebtn);
+    const deletbtn = document.createElement('button');
     deletbtn.classList = "newdelete";
-    deletbtn.innerHTML = '<i class="fas fa-trash-alt"></i>'
+    deletbtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
     newdiv.appendChild(deletbtn);
     input_container.appendChild(newdiv);
 }
 
 function delete_task(e){ 
-    const item = e.target;  
+    const item = e.target;
     if(item.classList[0] === 'newdelete'){
         const task = item.parentElement;
         task.remove();
     }
     if(item.classList[0] === 'newadd'){
-        console.log(item);
         const task = item.parentElement;
         task.classList.toggle('checked')
+    }
+    if(item.classList[0] === 'changebtn'){
+        console.log(item);
     }
 }
